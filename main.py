c296eb082ada7e6bc5e799772cc8d902d386e8e3
Jinwoo Sung <jwsung91@gmail.com>
2025-02-01 09:58:18 +0900

    chore: add __main__

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from calculator import Calculator
 
-calc = Calculator()
+if __name__ == "__main__":
 
-print("Add: 1 + 2 = ", calc.add(1, 2))
-print("Subtract: 4 - 2 = ", calc.subtract(4, 2))
-print("Multiply: 2 * 5 = ", calc.multiply(2, 5))
-print("Divide: 10 / 2 = ", calc.divide(10, 2))
+    calc = Calculator()
+
+    print("Add: 1 + 2 = ", calc.add(1, 2))
+    print("Subtract: 4 - 2 = ", calc.subtract(4, 2))
+    print("Multiply: 2 * 5 = ", calc.multiply(2, 5))
+    print("Divide: 10 / 2 = ", calc.divide(10, 2))
